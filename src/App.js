f6332b510aa7c4a59de5d1a02981897378187389
Septian Adi Pratama <septianadip7998@gmail.com>
2023-12-11 04:47:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const category = "HOLIDAY";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="parentbox">
+      <div className="foto">
+        <img src="shoes.png" alt="OK" />
+      </div>
+      <div className="deskripsi">
+        <p className="cate">{category}</p>
+        <h1 className="title">Pinky Swagg</h1>
+        <p className="price">IDR 73.309.399</p>
+        <p className="info">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, laudantium ea? Mollitia est labore atque consequatur optio molestiae nam id ipsa eaque explicabo! Doloribus obcaecati voluptates enim odit sapiente molestiae quod dignissimos dolores quibusdam ex, natus dolorem quis, reiciendis nesciunt dolor voluptatibus officia et. Temporibus quae ipsum placeat, iusto inventore quas expedita voluptas, unde quam cumque doloremque. Consequatur exercitationem deserunt delectus sint, accusamus, beatae ratione nemo magni enim deleniti nisi!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
