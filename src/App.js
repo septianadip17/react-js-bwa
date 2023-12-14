79f921ef6b28aa344187e861ec89b43761a928d8
Septian Adi Pratama <septianadip7998@gmail.com>
2023-12-14 07:38:23 +0700

    conditional rendering & map array

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="parentbox">
       <FotoProduk />
-      <ProdukInfo name="Youdora Dora" category="LEABRAN" />
+      <ProdukInfo isDiscount="coming" name="Youdora Dora" category="LEABRAN" />
     </div>
   );
 }
@@ -17,14 +17,35 @@ function FotoProduk() {
   );
 }
 
+function CheckDiscount(props) {
+  const {isDiscount} = props;
+  if(isDiscount == "yes"){
+    return(
+      <p>Diskon 50% Off</p>
+    );
+  }else if(isDiscount == "coming"){
+    return(
+      <p>Akan ada diskon...</p>
+    );
+  }else{
+    return(
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+
 function ProdukInfo(props) {
-  const { category, name } = props;
+  const { category, name, isDiscount } = props;
+  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Tidak gerah"];
+  const listBenefits = benefits.map((itemBenefit) =>
+    <li>{itemBenefit}</li>)
   return (
     <div>
       <div className="deskripsi">
         <p className="cate">{category}</p>
         <h1 className="title">{name}</h1>
         <p className="price">IDR 73.309.399</p>
+        <CheckDiscount isDiscount={isDiscount}/>
         <p className="info">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
           laudantium ea? Mollitia est labore atque consequatur optio molestiae
@@ -36,6 +57,9 @@ function ProdukInfo(props) {
           delectus sint, accusamus, beatae ratione nemo magni enim deleniti
           nisi!
         </p>
+        <ul>
+          {listBenefits}
+        </ul>
         <a onClick={(e) => TambahCart(name, e)} href="#">Add to Cart</a>
       </div>
     </div>
